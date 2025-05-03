7aa9e48c2ded20316aeed9e2b8504d128d71cba8
Mohd Irfan <mohdirfan70097@gmail.com>
2025-05-04 02:04:02 +0530

    Updatng

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,  // Ensures new connection string parser
+            useUnifiedTopology: true,  // Uses the new server discovery and monitoring engine
+        });
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
-        process.exit(1);
+       
     }
 };
 
